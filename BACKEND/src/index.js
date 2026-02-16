a229e6e6d7964b8ade0eead2bcd484af9d5b5f2f
Mitesh Bharvad <miteshbharvad888@gmail.com>
2026-02-16 10:16:15 +0530

    util folder is developed for the custom error,async handler function  and api response

---
+++ BACKEND/src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is Running on the port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed !!! ", error);
+  });
+
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`app listening on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
+
+*/
